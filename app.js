8af28c24c0407ee4d5d13bdbbf09b90b25017ae0
katryo <katoryo55@gmail.com>
2018-10-27 12:42:30 -0700

    add angular

--- app.js
+++ app.js
@@ -15,7 +15,6 @@
 
 'use strict';
 
-// [START gae_node_request_example]
 const express = require('express');
 
 const app = express();
@@ -24,10 +23,16 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello, world!').end();
 });
 
-// Start the server
+app.get('/api/', (req, res) => {
+  res.status(200).send('Hello, api!').end();
+});
+
+app.get('/api/:id', (req, res) => {
+  res.status(200).send(`Hello, ${req.params.id}`).end();
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
-// [END gae_node_request_example]
