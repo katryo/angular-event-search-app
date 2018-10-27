8c5e3fb78fc0f5c18382bcdeb4a2bc4f45d4a1d2
katryo <katoryo55@gmail.com>
2018-10-27 14:38:40 -0700

    add event search

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventSearchComponent } from './event-search/event-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventSearchComponent
   ],
   imports: [
     BrowserModule,
