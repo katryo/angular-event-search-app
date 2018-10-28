71acb35e66746c5c4dcd346126672148a388bc59
katryo <katoryo55@gmail.com>
2018-10-27 21:50:43 -0700

    implement events api

--- app.js
+++ app.js
@@ -1,43 +1,64 @@
-'use strict';
+"use strict";
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').load();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").load();
 }
 
-const express = require('express');
-const querystring = require('querystring');
-const geohash = require('ngeohash');
+const express = require("express");
+const querystring = require("querystring");
+const geohash = require("ngeohash");
+const fetch = require("node-fetch");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send("Hello, world!")
+    .end();
 });
 
-app.get('/api/events', (req, res) => {
-  const lat = parseFloat(req.query.lat);
-  const lng = parseFloat(req.query.lng);
+async function fetchEvents(lat, lng, keyword) {
   const code = geohash.encode(lat, lng);
 
-  const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
+  const baseUrl = "https://app.ticketmaster.com/discovery/v2/events.json?";
 
   const qs = querystring.stringify({
     apikey: process.env.TICKET_CONSUMER_KEY,
-    keyword: "football",
-    radius: '10',
-    unit: 'mile',
+    keyword: keyword,
+    radius: "10",
+    unit: "miles",
     geoPoint: code
   });
   const url = baseUrl + qs;
-  res.status(200).send('Hello, api!' + url).end();
+
+  const response = await fetch(url);
+  const result = await response.json();
+  const events = result._embedded.events;
+  return events;
+}
+
+app.get("/api/events", (req, res) => {
+  const lat = parseFloat(req.query.lat);
+  const lng = parseFloat(req.query.lng);
+  fetchEvents(lat, lng, "football")
+    .then(events => {
+      res.status(200).json({ events: events, status: "success" });
+    })
+    .catch(e => {
+      res.status(500).json({ events: [], status: "failure" });
+    });
 });
 
-app.get('/api/:id', (req, res) => {
-  res.status(200).send(`Hello, ${req.params.id}`).end();
+app.get("/api/:id", (req, res) => {
+  res
+    .status(200)
+    .send(`Hello, ${req.params.id}`)
+    .end();
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+  console.log("Press Ctrl+C to quit.");
 });
