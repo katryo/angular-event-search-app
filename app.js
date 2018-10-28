6ad8f0bb6ad5b6c41aa523c194f567c3eb236395
katryo <katoryo55@gmail.com>
2018-10-27 21:15:05 -0700

    remove ayy yaml

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 'use strict';
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
+
 const express = require('express');
+const querystring = require('querystring');
+const geohash = require('ngeohash');
 
 const app = express();
 
@@ -9,7 +15,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/events', (req, res) => {
-  res.status(200).send('Hello, api!').end();
+  const lat = parseFloat(req.query.lat);
+  const lng = parseFloat(req.query.lng);
+  const code = geohash.encode(lat, lng);
+
+  const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
+
+  const qs = querystring.stringify({
+    apikey: process.env.TICKET_CONSUMER_KEY,
+    keyword: "football",
+    radius: '10',
+    unit: 'mile',
+    geoPoint: code
+  });
+  const url = baseUrl + qs;
+  res.status(200).send('Hello, api!' + url).end();
 });
 
 app.get('/api/:id', (req, res) => {
